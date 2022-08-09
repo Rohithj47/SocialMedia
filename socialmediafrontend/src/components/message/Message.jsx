f6733221a1244b105051a0566def3bf0f2474dfb
Rohithj47 <rohithjanardhan98@gmail.com>
2022-08-09 22:42:41 +0530

    Changes for deployment

--- socialmediafrontend/src/components/message/Message.jsx
+++ socialmediafrontend/src/components/message/Message.jsx
@@ -6,12 +6,15 @@ import axios from "axios";
 export default function Message({ message, own }) {
   const PF = process.env.REACT_APP_PUBLIC_FOLDER
   const [messageSender, setSender] = useState({})
+  
+  
   useEffect(() => {
     axios.get(`users?userId=${message.sender}`)
           .then(resData => setSender(resData.data))
           .catch(err => console.log(err))
   }, [message])
-  console.log(message)
+
+  
   return (
     <div className={own ? "message own" : "message"}>
       <div className="messageTop">
